40259367ca3d8c47e9c2360743783672ea1e8c92
domeki <domeki151190@gmail.com>
2021-10-15 14:03:30 -0400

    Agregar archivos de proyecto.

---
+++ demoGlcontrol/Casa.cs
@@ -0,0 +1,152 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace demoGlcontrol
+{
+    class Casa
+    {
+
+
+        private float ancho;
+        private float largo;
+        private float alto;
+        private Vector3 pos;
+        private Vector3 trasladar;
+        //public float angulo = 0;
+        public Casa(Vector3 posicion, float ancho, float largo, float alto) {
+
+            this.trasladar = new Vector3(0, 0, 0);
+            this.pos = posicion;
+            this.ancho = ancho;
+            this.largo = largo;
+            this.alto = alto;
+        }
+        public void dibujar()
+        {
+            GL.LoadIdentity();
+
+            GL.PushMatrix();
+            GL.Translate(this.trasladar);
+            // GL.Rotate(angulo, 0, 1, 0);
+            GL.Rotate(90, 0, 1, 0);
+            casa();
+           // angulo++;
+            GL.PopMatrix();
+        }
+
+
+        public void casa()
+        {
+
+            GL.Begin(PrimitiveType.Triangles);
+            GL.Color3(188 / 255.0f, 129 / 255.0f, 80 / 255.0f);
+            //TECHO DE LA CASA FRONTAL
+            GL.Vertex3(pos.X, pos.Y + alto, pos.Z-alto/2);
+            GL.Vertex3(pos.X, pos.Y, pos.Z);
+            GL.Vertex3(pos.X+largo, pos.Y + alto, pos.Z - alto / 2);
+
+            GL.Vertex3(pos.X, pos.Y, pos.Z);
+            GL.Vertex3(pos.X + largo, pos.Y + alto, pos.Z - alto / 2);
+            GL.Vertex3(pos.X+largo, pos.Y, pos.Z);
+            
+            //TECHO DE LA CASA TRASERA
+            GL.Vertex3(pos.X, pos.Y + alto, pos.Z - alto / 2);
+            GL.Vertex3(pos.X, pos.Y, pos.Z-ancho);
+            GL.Vertex3(pos.X+largo, pos.Y+alto, pos.Z - alto / 2);
+
+            GL.Vertex3(pos.X, pos.Y, pos.Z - ancho);
+            GL.Vertex3(pos.X + largo, pos.Y + alto, pos.Z - alto / 2);
+            GL.Vertex3(pos.X + largo, pos.Y, pos.Z - ancho);
+            GL.Color3(234 / 255.0f, 205 / 255.0f, 143 / 255.0f);
+            
+            //TECHO DE LA CASA CARA IZQUIERDA
+            GL.Vertex3(pos.X, pos.Y + alto, pos.Z - alto / 2);
+            GL.Vertex3(pos.X, pos.Y, pos.Z);
+            GL.Vertex3(pos.X, pos.Y, pos.Z - ancho);
+            //TECHO DE LA CASA CARA DERECHA
+            GL.Vertex3(pos.X+largo, pos.Y + alto, pos.Z - alto / 2);
+            GL.Vertex3(pos.X + largo, pos.Y, pos.Z);
+            GL.Vertex3(pos.X + largo, pos.Y, pos.Z-ancho);
+
+            //PUERTA
+            GL.Color3(143 / 255.0f, 105 / 255.0f, 39 / 255.0f);
+            GL.Vertex3(pos.X + ancho / 3, pos.Y - alto / 2, pos.Z);
+            GL.Vertex3(pos.X + ancho / 3, pos.Y - alto, pos.Z);
+            GL.Vertex3(pos.X + ancho / 6, pos.Y - alto / 2, pos.Z);
+
+            GL.Vertex3(pos.X + ancho / 3, pos.Y - alto, pos.Z);
+            GL.Vertex3(pos.X + ancho / 6, pos.Y - alto / 2, pos.Z);
+            GL.Vertex3(pos.X + ancho / 6, pos.Y - alto, pos.Z);
+
+
+            //BASE DE LA CASA
+
+            //Cara Frontal
+            GL.Color3(93 / 255.0f, 109 / 255.0f, 126 / 255.0f);
+            GL.Vertex3(pos.X, pos.Y, pos.Z);//0,0,0
+            GL.Vertex3(pos.X, pos.Y - alto, pos.Z);//0,-5,0
+            GL.Vertex3(pos.X + largo, pos.Y, pos.Z);//8,0,0
+
+            GL.Vertex3(pos.X, pos.Y - alto, pos.Z);//0,-5,0
+            GL.Vertex3(pos.X + largo, pos.Y, pos.Z);//8,0,0
+            GL.Vertex3(pos.X + largo, pos.Y - alto, pos.Z);//,8,-5,0
+
+
+
+            //Cara de Atras
+            GL.Color3(231 / 255.0f, 76 / 255.0f, 60 / 255.0f);
+            GL.Vertex3(pos.X, pos.Y, pos.Z - ancho);
+            GL.Vertex3(pos.X, pos.Y - alto, pos.Z - ancho);
+            GL.Vertex3(pos.X + largo, pos.Y, pos.Z - ancho);
+
+            GL.Vertex3(pos.X, pos.Y - alto, pos.Z - ancho);
+            GL.Vertex3(pos.X + largo, pos.Y, pos.Z - ancho);
+            GL.Vertex3(pos.X + largo, pos.Y - alto, pos.Z - ancho);
+
+
+            //cara izquierda
+            GL.Color3(118 / 255.0f, 215 / 255.0f, 196 / 255.0f);
+            GL.Vertex3(pos.X, pos.Y, pos.Z);
+            GL.Vertex3(pos.X, pos.Y - alto, pos.Z);
+            GL.Vertex3(pos.X, pos.Y, pos.Z - ancho);
+
+            GL.Vertex3(pos.X, pos.Y, pos.Z - ancho);
+            GL.Vertex3(pos.X, pos.Y - alto, pos.Z);
+            GL.Vertex3(pos.X, pos.Y - alto, pos.Z - ancho);
+
+            //cara derecha
+            GL.Color3(244 / 255.0f, 208 / 255.0f, 63 / 255.0f);
+            GL.Vertex3(pos.X + largo, pos.Y, pos.Z);
+            GL.Vertex3(pos.X + largo, pos.Y - alto, pos.Z);
+            GL.Vertex3(pos.X + largo, pos.Y, pos.Z - ancho);
+
+            GL.Vertex3(pos.X + largo, pos.Y, pos.Z - ancho);
+            GL.Vertex3(pos.X + largo, pos.Y - alto, pos.Z);
+            GL.Vertex3(pos.X + largo, pos.Y - alto, pos.Z - ancho);
+            //cara superior
+            GL.Vertex3(pos.X, pos.Y, pos.Z - ancho);
+            GL.Vertex3(pos.X, pos.Y, pos.Z);
+            GL.Vertex3(pos.X + largo, pos.Y, pos.Z);
+
+            GL.Vertex3(pos.X + largo, pos.Y, pos.Z);
+            GL.Vertex3(pos.X, pos.Y, pos.Z - ancho);
+            GL.Vertex3(pos.X + largo, pos.Y, pos.Z - ancho);
+            //cara inferior
+            GL.Vertex3(pos.X, pos.Y - alto, pos.Z - ancho);
+            GL.Vertex3(pos.X, pos.Y - alto, pos.Z);
+            GL.Vertex3(pos.X + largo, pos.Y - alto, pos.Z);
+
+            GL.Vertex3(pos.X + largo, pos.Y - alto, pos.Z);
+            GL.Vertex3(pos.X, pos.Y - alto, pos.Z - ancho);
+            GL.Vertex3(pos.X + largo, pos.Y - alto, pos.Z - ancho);
+            GL.End();
+
+        }
+    }
+}
---
+++ demoGlcontrol/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace demoGlcontrol
+{
+    public partial class Form1 : Form
+    {
+        Casa casa = new Casa(new Vector3(0,0,0),5,8,5);
+        GLControl miglControl;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.miglControl = new GLControl();//inicializa
+            this.miglControl.BackColor = System.Drawing.Color.CadetBlue;
+            this.miglControl.Location = new System.Drawing.Point(0, 0);
+            this.miglControl.Name = "miglControl";
+            this.miglControl.Size = new System.Drawing.Size(this.Width, this.Height);
+            this.miglControl.TabIndex = 1;
+            this.miglControl.VSync = false;
+            this.miglControl.Load += new System.EventHandler(this.renderCanvas_Load);
+            this.miglControl.Paint += new System.Windows.Forms.PaintEventHandler(this.renderCanvas_Paint);
+            this.miglControl.Resize += new System.EventHandler(glControl1_Resize);
+            this.Controls.Add(this.miglControl);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void renderCanvas_Paint(object sender, PaintEventArgs e)
+        {
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            casa.dibujar();
+
+            miglControl.SwapBuffers();
+        }
+
+        private void renderCanvas_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(226 / 255.0f, 223 / 255.0f, 220 / 255.0f, 0.0f);    
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            miglControl.MakeCurrent();
+            PaintEventArgs p = null;
+            renderCanvas_Paint(sender, p);
+            GL.End();
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            int w = miglControl.Width;
+            int h = miglControl.Height;
+            miglControl.MakeCurrent();
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+
+            GL.Ortho(-10.0, 10.0, -10.0, 10.0, 10.0, -10.0);
+            // GL.Ortho(-50.0, 50.0, -50.0, 50.0, -100.0, 100.0);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.Viewport(0, 0, w, h);
+            GL.End();
+            miglControl.SwapBuffers();
+            
+        }
+    }
+    
+}
